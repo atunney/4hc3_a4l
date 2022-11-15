49cd4f07b667bb39b58b8cd16487f90871c4a095
Alyssa Tunney <tunneya@mcmaster.ca>
2022-11-15 03:00:22 -0500

    Updated login page elements

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+// https://codepen.io/opensoorce/pen/KQmvdL
+
 $(document).ready(function() {
 
     $('#submit').click(function(e) {
@@ -7,16 +9,8 @@ $(document).ready(function() {
         var validPassword = $('#password').val() === '123';
 
         if (validEmail === true && validPassword === true) {
-            // $('.valid').css('display', 'block');
-            //window.location = 
             window.location.href = "announcements.html";
         }
-        // else if (validEmail === true && validPassword === false) {
-
-        // }
-        // else if (validEmail === false && validPassword === true) {
-
-        // }
         else {
             $('.error').css('display', 'block');
         }
