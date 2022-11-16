0c20760cfa384567452eb4e384ddbb4ac349d808
Himanshu Aggarwal <aggarwal.h@outlook.com>
2022-11-15 21:53:40 -0500

    Change navbar selected item dynamically

--- js/navbar.js
+++ js/navbar.js
@@ -1,52 +1,70 @@
+const paths = {
+  announcements: "/announcements.html",
+  content: "/content.html",
+  discussions: "/discussions.html",
+  calendar: "/calendar.html",
+  profile: "/",
+  settings: "/",
+};
+
 $(function () {
+  const pathname = window.location.pathname;
   $("nav").append(
     `
-        <div class="navbar-item selected">
+        <a href="${paths.announcements}" class="navbar-item${
+          pathname === paths.announcements ? " selected" : ""
+        }">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon">
               <i class="fa-solid fa-bullhorn scale-110"></i>
             </div>
-            <a href="/announcements.html" class="navbar-text">Announcements</a>
+            <span class="navbar-text">Announcements</span>
           </div>
-        </div>
-        <div class="navbar-item">
+        </a>
+        <a href="${paths.content}" class="navbar-item${
+          pathname === paths.content ? " selected" : ""
+        }">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon">
               <i class="fa-solid fa-book-open scale-110"></i>
             </div>
-            <a href="/content.html" class="navbar-text">Content</a>
+            <span class="navbar-text">Content</span>
           </div>
-        </div>
-        <div class="navbar-item">
+        </a>
+        <a href="${paths.discussions}" class="navbar-item${
+          pathname === paths.discussions ? " selected" : ""
+        }">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon">
               <i class="fa-solid fa-comment scale-110"></i>
             </div>
-            <a href="/discussions.html" class="navbar-text">Discussions</a>
+            <span class="navbar-text">Discussions</span>
           </div>
-        </div>
-        <div class="navbar-item">
+        </a>
+        <a href="${paths.calendar}" class="navbar-item${
+          pathname === paths.calendar ? " selected" : ""
+        }">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon">
               <i class="fa-solid fa-calendar scale-110"></i>
             </div>
-            <a href="/calendar.html" class="navbar-text">Calender</a>
+            <span class="navbar-text">Calender</span>
           </div>
-        </div>
-        <div class="navbar-item">
+        </a>
+        <a href="${paths.profile}" class="navbar-item">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon bg-gray-200 rounded-xl mr-2">HA</div>
-            <a href="/" class="navbar-text">Himanshu</a>
+            <span class="navbar-text">Himanshu</span>
           </div>
-        </div>
-        <div class="navbar-item">
+        </a>
+        <a href="${paths.settings}" class="navbar-item">
           <div class="navbar-item-content-wrapper">
             <div class="navbar-icon">
               <i class="fa-solid fa-gear scale-125"></i>
             </div>
-            <a href="/" class="navbar-text">Settings</a>
+            <span class="navbar-text">Settings</span>
           </div>
-        </div>
+        </a>
           `
   );
 });
